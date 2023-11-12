637865c1df11f6300a81a1645a4de44e2915468b
Alexander <caminante.msk@gmail.com>
2023-11-12 14:49:08 -0300

    only desktop

--- src/components/productHead/ProductHead.js
+++ src/components/productHead/ProductHead.js
@@ -1,52 +1,41 @@
 import './ProductHead.css'
-import { AiOutlineSearch } from 'react-icons/ai'
-// import { useLocation } from 'react-router-dom'
 import {useState, useEffect} from 'react'
 import 'animate.css'
 
 const ProductHead = ({dataToRender}) => {
 
-  // const [animated, setAnimated] = useState(false)
-
   const [typeSelected, setTypeSelected] = useState("Todos")
+
+  const categories = ["Todos", "Eletrodomésticos", "Cozinha", "Banheiro", "Decoração", "Móveis"]
 
   const changeTypeHandler = (e) => {
     setTypeSelected(e.target.innerText)
-    // setAnimated(true)
   }
 
   useEffect(() => {
     dataToRender(typeSelected)
   }, [typeSelected])
 
+  const productCategories = () => {
+    return categories.map(item => {
+      return (
+        <p 
+          className={typeSelected === item ? 'link-product active' : "link-product"}
+          onClick={changeTypeHandler}
+          key={item}
+        >
+          {item}
+        </p>
+        )
+      }   
+    )
+  }
+
   return (
     <div className="productHead">
       <div className="container">
         <div className="toggle-product">
-          <p 
-            className={typeSelected === "Todos" ? 'link-product active' : "link-product"}
-            onClick={changeTypeHandler}
-          >
-            Todos
-          </p>
-          <p 
-            className={typeSelected === "Apartamento" ? 'link-product active' : "link-product"}
-            onClick={changeTypeHandler}
-          >
-            Apartamento
-          </p>
-          <p 
-            className={typeSelected === "Casa" ? 'link-product active' : "link-product"}
-            onClick={changeTypeHandler}
-          >
-            Casa
-          </p>
-          <p 
-            className={typeSelected === "Kitnet" ? 'link-product active' : "link-product"}
-            onClick={changeTypeHandler}
-          >
-            Kitnet
-          </p>
+          {productCategories()}
         </div>     
       </div>     
     </div>
