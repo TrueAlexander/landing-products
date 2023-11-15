b7f97c397ee6b43f200a5672177ab506d7aa1752
Alexander <caminante.msk@gmail.com>
2023-11-15 19:50:10 -0300

    finished first edition

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,94 +1,17 @@
-import React, {useState, useEffect} from 'react'
-import {HiOutlineMenuAlt4} from 'react-icons/hi'
-import {FaRegTimesCircle} from 'react-icons/fa'
 import {BsFillHouseFill} from 'react-icons/bs'
-// import { Link } from 'react-router-dom';
-// import { Link as LinkRoll } from 'react-scroll'
-import 'animate.css'
+
 
 import './Navbar.css'
 
 const Navbar = ({fullNav}) => {
 
-    // const purpose = "Todos"
-    // const type = "Todos"
-    // const zone = "Todos"
 
-    // const [burger, setBurger] = useState(false)
-    
-    // const burgerClick = () => {
-    //     setBurger(!burger)   
-    // }
-
-    // useEffect(() => {
-    //     burger && window.innerWidth < 940 ? document.body.style.overflow = 'hidden' : document.body.style.overflow = 'auto' 
-    //   }, [burger])
-
-    // const fillNav = () => {
-    //     const ancors = [{title:"Sobre nós", anchor:"about"}, {title:"Opiniões", anchor:"opinions"}]
-    //      return ( fullNav ? ancors.map((item, index) => {
-    //         return (
-    //             <li className="bold" key={index}><LinkRoll
-    //                                         onClick={burgerClick} 
-    //                                         className='scrollLink' 
-    //                                         to={item.anchor} 
-    //                                         activeClass="active" 
-    //                                         spy={true} 
-    //                                         smooth={true} 
-    //                                         offset={-100} 
-    //                                         duration={500} 
-    //                                     >{item.title}
-    //                                 </LinkRoll>
-    //                         </li> 
-    //                 )}) : " "
-    //             ) 
-    // }
 
     return (
         <div className='navbar' style={fullNav ? {position: "absolute"} : {position: "static"}}>
             <div className='container'>
                 <h1><span><BsFillHouseFill />Catálogo</span> de Produtos</h1>
-                {/* <ul 
-                    className={burger && window.innerWidth < 940
-                        ? 'nav-menu active animate__animated animate__fadeIn' 
-                        : 'nav-menu'
-                    }
-                >
-                    <li className="bold">
-                        <Link 
-                            to='/'
-                            onClick={burgerClick}
-                            >Home
-                        </Link>
-                    </li>
-                    <li className="bold"       
-                        ><Link 
-                            to='/imoveis'
-                            state={{
-                                purpose: {purpose},
-                                type: {type},
-                                zone: {zone},
-                              }}
-                            onClick={burgerClick}
-                            >Imóveis
-                        </Link>
-                    </li>
-                    {fillNav()}
-                    <li className="bold"><LinkRoll 
-                            onClick={burgerClick}
-                            className='scrollLink' 
-                            to='contacts' 
-                            activeClass="active" 
-                            spy={true} 
-                            smooth={true} 
-                            offset={-100} 
-                            duration={500}
-                        >Contatos</LinkRoll>
-                    </li>
-                </ul>
-                <div className='hamburger' onClick={burgerClick}>
-                    {burger ? (<FaRegTimesCircle className='icon' />) : (<HiOutlineMenuAlt4 className='icon' />)}
-                </div> */}
+
             </div>
         </div>
     )
--- src/components/productHead/ProductHead.js
+++ src/components/productHead/ProductHead.js
@@ -1,20 +1,31 @@
 import './ProductHead.css'
 import {useState, useEffect} from 'react'
 import 'animate.css'
+import { Link } from 'react-router-dom'
+import { Link as LinkRoll } from 'react-scroll'
+import {HiOutlineMenuAlt4} from 'react-icons/hi'
+import {FaRegTimesCircle} from 'react-icons/fa'
 
 const ProductHead = ({dataToRender}) => {
 
   const [typeSelected, setTypeSelected] = useState("Todos")
+  const [burger, setBurger] = useState(false)
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth)
 
   const categories = ["Todos", "Eletrodomésticos", "Cozinha", "Banheiro", "Decoração", "Móveis"]
 
   const changeTypeHandler = (e) => {
     setTypeSelected(e.target.innerText)
+    setBurger(false)
   }
 
   useEffect(() => {
     dataToRender(typeSelected)
   }, [typeSelected])
+
+  useEffect(() => {
+
+  }, [innerWidth])
 
   const productCategories = () => {
     return categories.map(item => {
@@ -31,12 +42,32 @@ const ProductHead = ({dataToRender}) => {
     )
   }
 
+  const burgerClick = () => {
+      setBurger(!burger)   
+  }
+
+  useEffect(() => {
+      burger && window.innerWidth < 940 ? document.body.style.overflow = 'hidden' : document.body.style.overflow = 'auto' 
+    }, [burger])
+
   return (
     <div className="productHead">
       <div className="container">
         <div className="toggle-product">
           {productCategories()}
-        </div>     
+        </div>
+          <div className='hamburger' onClick={burgerClick}>
+          {burger ? (<FaRegTimesCircle className='icon' />) : (<HiOutlineMenuAlt4 className='icon' />)}
+        </div>
+        {burger && <ul 
+            className={burger 
+                ? 'nav-menu active animate__animated animate__fadeIn' 
+                : 'nav-menu'
+            }
+        >
+          {productCategories()}  
+        </ul>}
+      
       </div>     
     </div>
   )
